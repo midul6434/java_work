ec45e878f74913a21f92886e52b2a28b20f055c5
Midul Talukder <midul@Miduls-MBP.home>
2019-07-24 00:55:01 -0400

    first commit

---
+++ BooleanTrueFalse.java
@@ -0,0 +1,15 @@
+public class BooleanTrueFalse{
+
+public static void main(String[]args)
+
+{ boolean MidulIsBad=true;
+boolean AkterIsOld=true;
+
+System.out.println(MidulIsBad);
+System.out.println(AkterIsOld);
+}
+
+
+
+}
+
